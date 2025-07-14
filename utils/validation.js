4a8d368c2070b3956c067349299e5a0e39dc27b0
Yonatan Gorovetz <goro5357@gmail.com>
2025-07-14 17:05:20 +0300

    started validations

--- utils/validation.js
+++ utils/validation.js
@@ -1,2 +1,14 @@
 //validate each argument
 //add validate all args function
+
+export function validateAction(command){
+    let commandSet = new Set(["add", "list", "search", "delete", "help"])
+    command.toLowerCase()
+    if(!commandSet.has(command)){
+        throw new Error(`Unknown command ${command}`)
+    }
+}
+
+function validateEmail(email){
+    
+}
