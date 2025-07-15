0829d1168950698a403cc0d9e01c38c066703d92
Yonatan Gorovetz <goro5357@gmail.com>
2025-07-15 15:20:32 +0300

    initial app file

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
 import { getFileData, updateFile } from "./utils/fileUtils.js"
+import validateParams from "./utils/validation.js"
 
 const params = {
-    action: process.argv[2],
+    command: process.argv[2],
     cname: process.argv[3],
     email: process.argv[4],
     phone: process.argv[5],
 }
 
+function run(){
+    try{
+        validateParams(params)
+    } catch(err){
+        console.log(`❌ Error: ${err.message}`)
+    }
+
+    if(params.command === "help"){
+        displayHelp()
+    }
+
+    let contacts = getFileData()
+
+    switch(params.command){
+        case "add":
+            addContact(contacts, params)
+            updateFile(contacts)
+            break;
+        case "list":
+            list(contacts)
+            break;
+        case "search":
+            search(contacts, params.cname)
+            break;
+        case "delete":
+            deleteContact(contacts, params.cname)
+            updateFile(contacts)
+            break;
+    }
+}
 
 
-//validate params
-//validate file
+run()
