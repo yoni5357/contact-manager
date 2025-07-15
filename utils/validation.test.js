ec171953f4c2ddc68bc8f16ff93a7453dfc2d80c
Yonatan Gorovetz <goro5357@gmail.com>
2025-07-15 17:32:11 +0300

    more validation tests

--- utils/validation.test.js
+++ utils/validation.test.js
@@ -1,8 +1,9 @@
-import { validateCommand, validateName, validateEmail, validatePhone, validateParams } from '../utils/validation.js';
+import { validateCommand, validateName, validateEmail, validatePhone, validateParams } from './validation.js';
 
 describe('validation module tests', () =>{
     it('should throw error for invalid command', () => {
         const params = { command: 'invalidCommand' };
         expect(() => validateCommand(params.command)).toThrow(`Unknown command ${command}`);
+        expect(() => validateCommand('')).toThrow(`Command is required`);
     })
 })
