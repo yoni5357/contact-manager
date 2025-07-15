fd6f3d95d083b1af4679f0fba70d666596fec002
TomerDun <tomer@dunsky.name>
2025-07-15 13:42:55 +0300

    Added list function

--- commands/commandHandler.js
+++ commands/commandHandler.js
@@ -5,8 +5,19 @@ import { updateFile } from "../utils/fileUtils";
 // All functions should get contcacts object as argument.
 // All functions assume that all data recieved is already validated.
 
+
+// --Edit contacts--
+
 function addContact(contacts, newContact) {
     contacts.push(newContact);
     console.log('Contact added: ', newContact.name);
     return true;    
+}
+
+// -- Show Contacts --
+function listContacts(contacts) {
+    console.log('=== All Contacts ===');
+    for (let i in contacts) {
+        console.log(`${i}. ${contacts[i].name} - ${contacts[i].email} - ${contacts[i].phone}`);        
+    }    
 }
