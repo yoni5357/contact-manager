d2f034d4b8a27f554dffd8ff411bcc6b60ae8d75
TomerDun <tomer@dunsky.name>
2025-07-15 13:25:53 +0300

    Added some instructions

--- commands/commandHandler.js
+++ commands/commandHandler.js
@@ -1,1 +1,4 @@
 //add, list, search, delete, help functions
+
+// All functions should get contcacts object as argument
+// All functions that edit the data should return the new object and not change the object itself.
